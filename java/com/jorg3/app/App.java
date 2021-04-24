afa75deee20010fe568147a3fb7550fa7a5b6fa1
jemanzano <jorgemanzano28@gmail.com>
2021-04-23 21:29:33 -0500

    NOmbrePoryecto: version final my app

---
+++ java/com/jorg3/app/App.java
@@ -0,0 +1,13 @@
+package com.jorg3.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
